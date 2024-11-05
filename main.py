58cd5fe4905a7d3f9def7f4eac6a540fa0807876
Username387J <jdeleijagbulu@gmail.com>
2024-11-04 19:40:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import pgzrun
+from random import randint
 
 WIDTH=300
 HEIGHT=300
 
+
 TITLE="Rectangle Pattern"
 
 def draw():
-    screen.fill(color="red")
+    screen.fill(color="black")
+    width=WIDTH
+    height=HEIGHT-200
+
+    r=255
+    g=0
+    b=randint(120,255)
+
+    for i in range(20):
+        myRect=Rect((0,0),(width,height))
+        myRect.center=150,150
+        screen.draw.rect(myRect,(r,g,b))
+        width=width-10
+        height=height+10
+        r=r-10
+        g=g+10
 
 
 
